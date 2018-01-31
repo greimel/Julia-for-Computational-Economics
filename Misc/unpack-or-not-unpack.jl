94889d10029010b73c481ac39ea0b89d345e5c94
fabgrei <fabgrei@users.noreply.github.com>
2018-01-31 20:37:54 +0100

    Update unpack-or-not-unpack.jl

--- Misc/unpack-or-not-unpack.jl
+++ Misc/unpack-or-not-unpack.jl
@@ -1,4 +1,4 @@
-## Julia has a fantastic `Parameters.jl` package. It makes the code cleaner, without losing performance.
+## Julia has the `Parameters.jl` package. It encourages bundling parameters together. This makes the code cleaner, without losing performance.
 
 using Parameters
 
@@ -8,7 +8,7 @@ type Par{T <: Real}
 end
 
 ### Implementation 1: unpack in outer function
-function f1(x::T, y::T, α::T, β::T) where T <: Real
+@inline function f1(x::T, y::T, α::T, β::T) where T <: Real
     x^α * y^β
 end
 
@@ -16,12 +16,12 @@ function g1(xx::Array{T}, yy::Array{T}, α::T, β::T) where T <: Real
     mean(f1.(xx, yy, α, β))
 end
 
-function h1(n::Int, p::Par{Float64})
+function h1!(xx::Array{Float64}, yy::Array{Float64}, n::Int, p::Par{Float64})
     @unpack α, β = p
     out = 0.0
     for i in 1:n
-        xx = rand(500, 500)
-        yy = rand(500, 500)
+        xx .= rand()
+        yy .= rand()
         out += g1(xx, yy, α, β)/n
     end
 end
@@ -37,14 +37,26 @@ function g2(xx::Array{T}, yy::Array{T}, p::Par{T}) where T <: Real
     mean(f2.(xx, yy, p))
 end
 
-function h2(n::Int, p::Par{Float64})
+function h2!(xx::Array{Float64}, yy::Array{Float64}, n::Int, p::Par{Float64})
     out = 0.0
     for i in 1:n
-        xx = rand(500, 500)
-        yy = rand(500, 500)
+        xx .= rand()
+        yy .= rand()
         out += g2(xx, yy, p)/n
     end
 end
 
 
 p = Par(0.1, 0.2)
+
+xx = zeros(500,500)
+yy = zeros(500,500)
+
+## compile on first run
+@time h1!(xx, yy, 10, p)
+@time h2!(xx, yy, 10, p)
+## runs approximately 1 second each
+gc()
+@time h1!(xx, yy, 20, p)
+gc()
+@time h2!(xx, yy, 20, p)
